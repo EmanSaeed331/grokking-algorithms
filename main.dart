26527ced81d543ac87919c806f4ffa68bdd7874c
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2021-12-12 22:38:28 +0200

    selecting sort added

--- main.dart
+++ main.dart
@@ -1,7 +1,12 @@
-import 'binary_search.dart';
+import 'chapter_2/selecting_sort.dart';
 
 void main() {
-  dynamic List = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  String x = binarySearch(List, 6);
-  print(x);
+  /* binary search */
+  //  dynamic List = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  //String x = binarySearch(List, 6);
+  //print(x);
+  /* selection sort */
+  List<int> arr = [8, 2, 3, 1, 5, 6, 7, 8, 0];
+  List<int> arr2 = selectionSort(arr);
+  print(arr2);
 }
