7cf451ad811fc5ce26000d04cd70b4d6f659997d
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2021-12-25 01:31:24 +0200

    recursion diffrent types added

--- chapter_3/recursion.dart
+++ chapter_3/recursion.dart
@@ -1,3 +1,43 @@
+/*There are 4 type of recursion :
+  1- Direct recusion -> call the function itself .
+  2- non direct recursion -> call another function .
+  3- tail recursion -> the last call is the recrsive function .
+  4- non tail recursion  -> the last call is not recursive function . 
+*/
+
+// 1- Direct Function
+countDown(i) {
+  return countDown(i - 1);
+}
+
+//2- non Direct Function
+void even() {
+  odd();
+}
+
+void main() {
+  even();
+}
+
+//Tail Recursion
+int Tail_Recursion(int i) {
+  if (i == 1) {
+    return 1;
+  }
+  return i * Tail_Recursion(i - 1);
+}
+
+void NonTailRecursion(int i) {
+  if (i == 0) {
+    print(1);
+  }
+
+  NonTailRecursion(i - 1);
+  print(i);
+}
+
+void odd() {}
+
 void countdown(i) {
   print(i);
   if (i <= 0) {
--- test.py
+++ test.py
@@ -1,16 +1,9 @@
-num =[3]
-num2 =[-2,-1]
-merged_arr =sorted(num+num2)
-length = len(merged_arr)
-print(merged_arr)
-
-median_e =  (merged_arr[len(merged_arr)//2] + merged_arr[len(merged_arr)//2-1]) / 2
-median_o = (length +1) / 2
-if(len(merged_arr ) == 1 ):
-    print(merged_arr[0])
-elif (length % 2 ==0):
-    print (median_e)
-       
-else:
-    print(median_o)
-
+a , h = list(map(int, input().split()))
+sum = 0
+for i in range (0,a):
+    b = int(input())
+    if b <= h :
+        sum+=1
+    else:
+        sum+=2
+print(sum)
