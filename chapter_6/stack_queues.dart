282de1c5da3599220c0ddc215e7b5e7b1ffffa22
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-01-05 14:38:52 +0200

    add: queue

---
+++ chapter_6/stack_queues.dart
@@ -0,0 +1,18 @@
+import 'dart:collection';
+
+void myQueueMethod (){
+  final myQueue = Queue<String>();
+
+  myQueue.add('First element');
+  myQueue.add('Second element');
+  myQueue.add('third element');
+  print(myQueue);
+  myQueue.remove('First element');
+  print(myQueue);
+}
+void myStackMethod (){
+  final myStack = Stack
+}
+void main() {
+  
+}
